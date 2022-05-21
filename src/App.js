c03d880b299b4456607282e39bcd604bea0baecb
redwansikder <57248683+redwansikder@users.noreply.github.com>
2022-05-21 14:12:37 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NavBar from './Components/NavBar'
 import Main from './Components/Main'
 import Footer from './Components/Footer'
 
-function Final() {
+export default function App() {
   return (
     <div>
       <NavBar />
@@ -16,4 +16,3 @@ function Final() {
   )
 }
 
-ReactDOM.render(<Final />, document.getElementById('root'))
